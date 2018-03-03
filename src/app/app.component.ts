bc7a4f82d611540834f6744e2c265debbd8770b4
Connor <connormakhlouta@gmail.com>
2018-03-03 20:36:05 +0200

    feat(table): add table population

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  columns = [
+    {type: 'number', name: 'Cool Nums', binder: 'nums' },
+    {type: 'text', name: 'Cool Strings', binder: 'strs' },
+    {type: 'number', name: 'Cool Nums', binder: 'nums2' },
+    {type: 'number', name: 'Cool Nums', binder: 'nums3' },
+    {type: 'number', name: 'Cool Nums', binder: 'nums4' },
+    ]
+
+  rows = [
+    {nums: 5, strs: 'yooyo', nums2: 33, nums3: 44, nums4: 55},
+    {nums: 5, strs: 'yooyo', nums2: 33, nums3: 44, nums4: 55},
+    {nums: 5, strs: 'yooyo', nums2: 33, nums3: 44, nums4: 55},
+    {nums: 5, strs: 'yooyo', nums2: 33, nums3: 44, nums4: 55}
+  ]
 }
--- src/app/modules/smarter-table/smarter-table.component.ts
+++ src/app/modules/smarter-table/smarter-table.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input, OnChanges} from '@angular/core';
 
 @Component({
   selector: 'smarter-table',
   templateUrl: './smarter-table.component.html',
   styleUrls: ['./smarter-table.component.css']
 })
-export class SmarterTableComponent implements OnInit {
+export class SmarterTableComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  can_filter = true
+  data = []
 
+  _columns = []
+  _rows = []
+  @Input() set columns(vall) {
+    this._columns = vall
+
+  }
+
+  @Input() set rows(value: array) {
+    this._rows = value
+  }
+
+
+  constructor() {}
   ngOnInit() {
+
+  }
+
+  ngOnChanges() {
+    this._columns && this._rows ? this.matchDataToColumns() : console.log('no rows or cols')
+  }
+
+  matchDataToColumns() {
+    this.data = this._rows.map(item => {
+      let row = new Array(this._columns.length)
+      for (let col in item) {
+        let index = this._columns.findIndex(item => {
+          return item.binder == col
+        })
+        row[index] = item[col]
+      }
+      return row
+    })
   }
 
 }
